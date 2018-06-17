baf8f43d7c13170a4fd5f9133a3282969aed40aa
KUROKI Shinsuke <s-kuroki@aiming-inc.com>
2018-06-17 15:34:41 +0900

    Add simple count implementation with firestore

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,31 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+admin.initializeApp(functions.config().firebase);
+var db = admin.firestore();
+
+let fetch = require('node-fetch');
+
+function sendToSlack(url, message) {
+  return fetch(url, { method: 'POST', headers: { "Content-Type": "application/json" }, body: JSON.stringify({ response_type: "in_channel", text: message }) });
+}
 
 exports.karimono = functions.https.onRequest((request, response) => {
- response.send("karimono!");
+  console.log(JSON.stringify(request.body));
+  var count = db.collection('karimono').doc('count');
+  count.get().then(doc => {
+    if (!doc.exists) {
+      return 0;
+    } else {
+      return doc.data().count + 1;
+    }
+  }).then(num => {
+    var body = 'karimono! ' + num.toString();
+    console.log('response_body: ' + body);
+    sendToSlack(request.body.response_url, body)
+    return
+  }).then(() => { return }).catch(() => { return });
+  return response.sendStatus(200);
 });
+
+console.log('ready');
